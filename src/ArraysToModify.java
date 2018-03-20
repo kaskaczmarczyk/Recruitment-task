5d23180985ebe94a14b1c094fe1dc41ae5767b6a
Kasia <kas.kaczmarczyk@gmail.com>
2018-03-20 12:40:49 +0100

    Recruitment task in the scope of operations on arrays.

---
+++ src/ArraysToModify.java
@@ -0,0 +1,78 @@
+public class ArraysToModify {
+    private int startIndex = 0;
+    public boolean ifArrayLengthEqualTwo(int tabA[]) {
+        int len = tabA.length;
+        if (len == 2) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int checkThreeElement(int tabA[], int tabB[]) {
+        int numberOfExchanges = 0;
+        while (tabA.length - startIndex > 2) {
+            int newTabA[] = new int[3];
+            newTabA[0] = tabA[startIndex];
+            newTabA[1] = tabA[startIndex+1];
+            newTabA[2] = tabA[startIndex+2];
+            int newTabB[] = new int[3];
+            newTabB[0] = tabB[startIndex];
+            newTabB[1] = tabB[startIndex+1];
+            newTabB[2] = tabB[startIndex+2];
+
+            if (!(newTabA[0] < newTabA[1]) && newTabB[0] < newTabA[1]) {    //jeśli warunek spełniony to możliwa zamiana elementów o ideksie 1
+                exchangeElment(newTabA, newTabB, 1);
+                updateArrays(tabA, tabB, newTabA, newTabB, 1);
+                numberOfExchanges++;
+                if (!(newTabA[1] < newTabA[2] && newTabB[1] < newTabA[2])) {       //jeśli warunek spełniony to możliwa zamiana elementów o ideksie 2
+                    exchangeElment(newTabA, newTabB, 2);
+                    updateArrays(tabA, tabB, newTabA, newTabB, 2);
+                    numberOfExchanges++;
+                }
+                showTabs(newTabA, newTabB);
+            }
+            if (newTabA[1] == newTabA[2] && newTabB[0] == newTabB[1] || newTabB[1] == newTabB[2] && newTabA[0] == newTabA[1]) {
+                exchangeElment(newTabA, newTabB, 1);
+                updateArrays(tabA, tabB, newTabA, newTabB, 1);
+                numberOfExchanges++;
+            }
+            System.out.println();
+            startIndex++;
+        }
+        return numberOfExchanges;
+    }
+
+    public void exchangeElment(int newTabA[], int newTabB[], int index) {
+        int tmp = newTabA[index];
+        newTabA[index] = newTabB[index];
+        newTabB[index] = tmp;
+    }
+
+    public void updateArrays(int tabA[], int tabB[], int newTabA[], int newTabB[], int index) {
+        tabA[index] = newTabA[index];
+        tabB[index] = newTabB[index];
+    }
+
+
+    public void showTabs(int tabA[], int tabB[]) {
+        for (int elementA: tabA) {
+            System.out.print(elementA + ", ");
+        }
+        System.out.println();
+        for (int elementB : tabB) {
+            System.out.print(elementB + ", ");
+        }
+    }
+
+    public int modifyArraysForTwoElements(int tabA[], int tabB[]) {
+        if (!(tabA[0] < tabA[1])){
+            exchangeElment(tabA, tabB, 0);
+            showTabs(tabA, tabB);
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+/*        int tabA[] = {5, 3, 7, 7, 10};
+        int tabB[] = {1, 6, 6, 9, 9};*/
+/*        int tabA[] = {5, 3};
+        int tabB[] = {1, 6};*/
+        int tabA[] = {5, 3, 7, 7, 10, 10, 11, 16, 16, 18, 23, 24, 28, 26};
+        int tabB[] = {1, 6, 6, 9, 9, 12, 15, 14, 20, 21, 20, 22, 25, 30};
+
+        ArraysToModify arrays = new ArraysToModify();
+        int numberOfActions;
+        if (arrays.ifArrayLengthEqualTwo(tabA)) {
+            numberOfActions = arrays.modifyArraysForTwoElements(tabA, tabB);
+        } else {
+            numberOfActions = arrays.checkThreeElement(tabA, tabB);
+        }
+        if (numberOfActions > 0.5 * tabA.length) {
+            numberOfActions = tabA.length - numberOfActions;
+        }
+        System.out.println("Number of exchanges: " + numberOfActions);
+    }
+}
